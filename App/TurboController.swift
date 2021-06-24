6e79e4f942d0e6c9998c0a7978dc76f5515cfbd2
Dale Zak <dalezak@gmail.com>
2021-06-24 00:01:53 -0600

    Initial Turbo-iOS base project

---
+++ App/TurboController.swift
@@ -0,0 +1,393 @@
+import UIKit
+import Turbo
+import WebKit
+import SwiftUI
+import SafariServices
+
+class TurboController: UINavigationController {
+
+    private lazy var tabBar = makeTabBar()
+    private lazy var session = makeSession()
+    private lazy var modalSession = makeSession()
+    private lazy var settings = loadSettings()
+    private lazy var processPool = WKProcessPool()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadNavBar()
+        loadTabBar()
+        loadTabs()
+        loadHome()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        tabBar.invalidateIntrinsicContentSize()
+    }
+    
+    func visit(url: URL, action: VisitAction = .advance, properties: PathProperties = [:]) {
+        if (presentedViewController != nil) {
+            dismiss(animated: true)
+        }
+        let viewController = VisitableViewController(url: url)
+        if url.path == "/", !viewControllers.isEmpty {
+            popViewController(animated: true)
+            viewControllers = Array() + [viewController]
+            session.visit(viewController)
+        } else if isModal(properties) {
+            present(viewController, animated: true)
+            modalSession.visit(viewController)
+        } else if isReplace(properties) {
+            viewControllers = Array(viewControllers.dropLast()) + [viewController]
+            session.visit(viewController)
+            session.reload()
+        } else if session.activeVisitable?.visitableURL == url {
+            let viewControllers = viewControllers.dropLast()
+            setViewControllers(viewControllers + [viewController], animated: false)
+            session.visit(viewController)
+        } else if action == .advance {
+            pushViewController(viewController, animated: true)
+            session.visit(viewController)
+        } else if action == .replace {
+            viewControllers = Array() + [viewController]
+            session.visit(viewController)
+        } else if action == .restore {
+            popViewController(animated: true)
+            session.visit(viewController)
+        } else {
+            pushViewController(viewController, animated: true)
+            session.visit(viewController)
+        }
+    }
+    
+    func loadHome() {
+        let url = turboUrl()
+        let properties = pathProperties(url)
+        visit(url: url, action: .replace, properties: properties)
+    }
+    
+    func loadNavBar() {
+        if let navbar = settings["navbar"] as? Dictionary<String, String> {
+            navigationBar.tintColor = UIColor(hexRGB: navbar["foreground"])
+            navigationBar.barTintColor = UIColor(hexRGB: navbar["background"])
+            navigationBar.titleTextAttributes = [.foregroundColor: UIColor(hexRGB: navbar["foreground"]) as Any]
+        }
+    }
+    
+    func loadTabBar() {
+        if let tabbar = settings["tabbar"] as? Dictionary<String, String> {
+            UITabBar.appearance().barTintColor = UIColor(hexRGB: tabbar["background"])
+            UITabBar.appearance().tintColor = UIColor(hexRGB: tabbar["selected"])
+            UITabBar.appearance().unselectedItemTintColor = UIColor(hexRGB: tabbar["unselected"])
+        }
+    }
+    
+    func loadTabs(_ authenticated: Bool = false) {
+        tabBar.items?.removeAll()
+        if let tabs = settings["tabs"] as? [Dictionary<String, AnyObject>] {
+            for (index, tab) in tabs.enumerated() {
+                let title = tab["title"] as? String
+                let image = tab["icon_ios"] as? String
+                let protected = tab["protected"] as? Bool ?? false
+                let tabBarItem = UITabBarItem(title: title, image: UIImage(systemName: image!), tag: index)
+                if ((protected && authenticated) || protected == false) {
+                    tabBar.items?.append(tabBarItem)
+                }
+            }
+            if (tabBar.items!.count > 0) {
+                tabBar.selectedItem = tabBar.items![0]
+            }
+        }
+        if (tabBar.items!.count > 0) {
+            tabBar.frame = CGRect(x: 0, y: self.view.frame.height - self.view.safeAreaInsets.bottom + 49.0, width: self.view.frame.width, height: 49)
+            tabBar.isHidden = false
+            tabBar.layer.zPosition = 0
+        }
+        else {
+            tabBar.frame = CGRect(x: 0, y: self.view.frame.height, width: self.view.frame.width, height: 0)
+            tabBar.isHidden = true
+            tabBar.layer.zPosition = -1
+        }
+    }
+    
+    func loadButtons(_ authenticated: Bool = false) {
+        if let buttons = settings["buttons"] as? [Dictionary<String, AnyObject>] {
+            let viewController = session.topmostVisitable!.visitableViewController
+            var leftBarButtonItems: [UIBarButtonItem] = []
+            var rightBarButtonItems: [UIBarButtonItem] = []
+            for (_, button) in buttons.enumerated() {
+                let path: String? = button["path"] as? String
+                if (path == session.webView.url?.path) {
+                    let title: String? = button["title"] as? String
+                    let icon: String? = button["icon_ios"] as? String
+                    let side: String? = button["side"] as? String
+                    let visit: String? = button["visit"] as? String
+                    let script: String? = button["script"] as? String
+                    let protected = button["protected"] as? Bool ?? false
+                    if ((protected && authenticated) || protected == false) {
+                        let button: UIBarButtonItem? = loadButton(title: title, icon: icon)
+                        if (button != nil) {
+                            button!.actionClosure = {
+                                if (visit != nil) {
+                                    let url = self.turboUrl(visit)
+                                    let properties = self.pathProperties(url)
+                                    self.visit(url: url, action: .replace, properties: properties)
+                                }
+                                else if (script != nil) {
+                                    self.session.webView.evaluateJavaScript(script!) { _, _ in }
+                                }
+                            }
+                            if (side == "left"){
+                                leftBarButtonItems.append(button!)
+                            }
+                            else if (side == "right") {
+                                rightBarButtonItems.append(button!)
+                            }
+                        }
+                    }
+                }
+            }
+            viewController.navigationItem.leftBarButtonItems = leftBarButtonItems
+            viewController.navigationItem.rightBarButtonItems = rightBarButtonItems
+        }
+    }
+    
+    func loadButton(title: String?, icon: String?) -> UIBarButtonItem? {
+        if (title != nil) {
+            return UIBarButtonItem(title: title!, style: .plain, target: nil, action: nil)
+        }
+        else if (icon != nil) {
+            let image = UIImage(systemName: icon!)
+            return UIBarButtonItem(image: image, style: .plain, target: nil, action: nil)
+        }
+        return nil;
+    }
+    
+    func loadSettings() -> Dictionary<String, AnyObject> {
+        let path = turboPath("/turbo.json")
+        let text = session.webView.evaluate("""
+          var request = new XMLHttpRequest();
+          request.open("GET",'\(path)',false);
+          request.send(null);
+          request.responseText;
+          """)
+        if let json = stringToDictionary(text: text) {
+            return (json["settings"] as? Dictionary<String, AnyObject>)!
+        }
+        return [:]
+    }
+    
+    func makeTabBar() -> UITabBar {
+        let tabBar = UITabBar()
+        tabBar.delegate = self
+        tabBar.frame = CGRect(x: 0, y: self.view.frame.height, width: self.view.frame.width, height: 0)
+        tabBar.items = []
+        self.view.addSubview(tabBar)
+        tabBar.translatesAutoresizingMaskIntoConstraints = false
+        tabBar.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        tabBar.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+        tabBar.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        return tabBar
+    }
+    
+    func makeSession() -> Session {
+        let configuration = WKWebViewConfiguration()
+        configuration.processPool = processPool
+        configuration.applicationNameForUserAgent = "Turbo-iOS"
+        let session = Session(webViewConfiguration: configuration)
+        session.delegate = self
+        session.webView.allowsLinkPreview = false
+        session.pathConfiguration = pathConfiguration
+        return session
+    }
+    
+    private lazy var pathConfiguration = PathConfiguration(sources: [
+        .server(turboUrl("/turbo.json")),
+        .file(Bundle.main.url(forResource: "Turbo", withExtension: "json")!)
+    ])
+    
+    func isModal(_ properties: PathProperties) -> Bool {
+        let presentation = properties["presentation"] as? String
+        return presentation == "modal"
+    }
+    
+    func isReplace(_ properties: PathProperties) -> Bool {
+        let presentation = properties["presentation"] as? String
+        return presentation == "replace"
+    }
+    
+    func isRestore(_ properties: PathProperties) -> Bool {
+        let presentation = properties["presentation"] as? String
+        return presentation == "restore"
+    }
+    
+    func pathProperties(_ url: URL) -> PathProperties {
+        return pathConfiguration.properties(for: url)
+    }
+    
+    func turboPath(_ path: String? = "") -> String {
+        let environment = ProcessInfo.processInfo.environment["ENVIRONMENT"]! as String
+        let backendURL = Bundle.main.infoDictionary?["TURBO_URL" as String] as! Dictionary<String, String>
+        return backendURL[environment]! + path!;
+    }
+    
+    func turboUrl(_ path: String? = "") -> URL {
+        return URL(string: turboPath(path))!
+    }
+    
+    func stringToDictionary(text: String) -> [String: Any]? {
+        if let data = text.data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
+    
+    func showAlert(title: String, error: Error) {
+        let alert = UIAlertController(title: title, message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true)
+    }
+    
+    func showError(title: String, error: Error, image: String = "exclamationmark.triangle") {
+        guard let topViewController = self.topViewController else { return }
+
+        let errorView = ErrorView(title: title, error: error.localizedDescription, image: image)
+        let hostingController = UIHostingController(rootView: errorView)
+
+        topViewController.addChild(hostingController)
+        hostingController.view.frame = topViewController.view.frame
+        topViewController.view.addSubview(hostingController.view)
+        hostingController.didMove(toParent: topViewController)
+    }
+}
+
+extension TurboController : UITabBarDelegate {
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        let tabs = settings["tabs"] as? [Dictionary<String, AnyObject>]
+        let tab = tabs![item.tag] as Dictionary<String, AnyObject>
+        let path = tab["visit"] as? String
+        let url = turboUrl(path)
+        let properties = pathProperties(url)
+        visit(url: url, action: .replace, properties: properties)
+    }
+}
+
+extension TurboController: SessionDelegate {
+    func session(_ session: Session, didProposeVisit proposal: VisitProposal) {
+        visit(url: proposal.url, action: proposal.options.action, properties: proposal.properties)
+    }
+    
+    func session(_ session: Session, openExternalURL url: URL) {
+        let safariViewController = SFSafariViewController(url: url)
+        present(safariViewController, animated: true, completion: nil)
+    }
+    
+    func session(_ session: Session, didFailRequestForVisitable visitable: Visitable, error: Error) {
+        if let turboError = error as? TurboError {
+            switch turboError {
+            case .http(let statusCode):
+                switch statusCode {
+                case 401:
+                    showError(title: "Login Required", error: turboError, image: "lock.shield")
+                case 404:
+                    showError(title: "Page Not Found", error: turboError, image: "questionmark.circle")
+                default:
+                    showError(title: "Problem Loading Page", error: turboError, image: "exclamationmark.triangle")
+                }
+            case .networkFailure:
+                showError(title: "Network Failure", error: turboError, image: "wifi.slash")
+            case .timeoutFailure:
+                showError(title: "Request Timeout", error: turboError, image: "clock")
+            case .contentTypeMismatch:
+                showError(title: "Content Type Mismatch", error: turboError, image: "nosign")
+            case .pageLoadFailure:
+                showError(title: "Problem Loading Page", error: turboError, image: "xmark.square")
+            }
+        }
+        else {
+            showError(title: "Problem Loading Page", error: error, image: "exclamationmark.triangle")
+        }
+    }
+    
+    func sessionDidFinishRequest(_ session: Session) {
+        let script = "document.querySelector(\"meta[name='turbo:authenticated']\").content"
+        session.webView.evaluateJavaScript(script, completionHandler: { (html: Any?, error: Error?) in
+            let authenticated = html as? String
+            self.loadTabs(authenticated == "true")
+            self.loadButtons(authenticated == "true")
+        })
+    }
+}
+
+extension WKWebView {
+    func evaluate(_ script: String) -> String {
+        var response = ""
+        var finished = false
+        evaluateJavaScript(script, completionHandler: { (result, error) in
+            if error == nil && result != nil {
+                response = result as! String
+            }
+            finished = true
+        })
+        while !finished {
+            RunLoop.current.run(mode: RunLoop.Mode(rawValue: "NSDefaultRunLoopMode"), before: NSDate.distantFuture)
+        }
+        return response;
+    }
+}
+
+extension UIBarButtonItem {
+    private struct AssociatedObject {
+        static var key = "action_closure_key"
+    }
+
+    var actionClosure: (()->Void)? {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedObject.key) as? ()->Void
+        }
+        set {
+            objc_setAssociatedObject(self, &AssociatedObject.key, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            target = self
+            action = #selector(didTapButton(sender:))
+        }
+    }
+
+    @objc func didTapButton(sender: Any) {
+        actionClosure?()
+    }
+}
+
+extension UIColor {
+    convenience init?(hexRGBA: String?) {
+        guard let rgba = hexRGBA, let val = Int(rgba.replacingOccurrences(of: "#", with: ""), radix: 16) else {
+            return nil
+        }
+        self.init(red: CGFloat((val >> 24) & 0xff) / 255.0, green: CGFloat((val >> 16) & 0xff) / 255.0, blue: CGFloat((val >> 8) & 0xff) / 255.0, alpha: CGFloat(val & 0xff) / 255.0)
+    }
+    
+    convenience init?(hexRGB: String?) {
+        guard let rgb = hexRGB else {
+            return nil
+        }
+        self.init(hexRGBA: rgb + "ff")
+    }
+}
+
+struct ErrorView: View {
+    let title: String
+    let error: String
+    let image: String
+
+    var body: some View {
+        VStack(spacing: 12) {
+            Image(systemName: image).resizable().scaledToFit().foregroundColor(.accentColor).frame(height: 40)
+            Text(title).font(.title).padding(.horizontal)
+            Text(error).multilineTextAlignment(.center).padding(.horizontal)
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,194 @@
-# turbo-ios-base
-Turbo-iOS base project that's entirely driven from your backend Rails app.
+# Turbo-iOS-Base
+### Turbo-iOS base project that's entirely driven from your backend Rails app.
+
+There were _four primary goals_ for this project:
+1. reusable base project that can be pointed to any Rails app
+2. app styling, tabs and navbar buttons driven from the server
+3. handle both authenicated and unauthenicated users
+4. no need to write any custom Swift code
+
+_Disclaimer_: It's been over six years since I've written anything in Objective-C, and I've never written any Swift prior to this project. So the code has lots of room for improvement, refactoring, cleanup, etc.
+
+If you haven't already, I _highly recommend_ you read the following articles about [Turbo-iOS](https://github.com/hotwired/turbo-ios).
+- [Hybrid iOS apps with Turbo by Joe Masilotti](https://masilotti.com/turbo-ios/hybrid-apps-with-turbo/)
+- [Drifting Ruby Turbo Native for iOS by David Kimura](https://www.driftingruby.com/episodes/turbo-native-for-ios)
+- [Native tab bar with Turbo-iOS by Bram Jetten](https://bramjetten.dev/articles/native-tab-bar-with-turbo-ios)
+
+#### Clone Repo
+Clone this repo locally to get started.
+```
+git clone https://github.com/dalezak/turbo-ios-base.git
+```
+
+#### Update Target Information
+- open **App.xcodeproj**
+- click on **App** project
+- select **App** under **Targets**
+- change **Display Name** to the name of your app
+- change **Bundle Identifier** to your reverse domain name
+
+#### Update Info.plist URLs
+- open **Info.plist** file
+- expand **TURBO_URL** item
+- change **development** to your local environment
+- change **production** to your production environment
+
+#### Replace Asset Images
+- visit **https://appicon.co**
+- upload **1024 x 1024** image
+- click **Generate** button
+- replace **Assets.xcassets** in the project with downloaded file
+
+#### Add Turbo Gem
+Add `turbo-rails` to your **Gemfile**.
+```
+gem "turbo-rails"
+```
+
+#### Import Turbo Javascript
+Add the following code to your **application.js** file.
+```
+import { Turbo } from "@hotwired/turbo-rails";
+window.Turbo = Turbo;
+```
+Add any custom javascript to **turbo/bridge.js** in your javascript folder.
+```
+export default class Bridge {
+  static sayHello() {
+    document.body.innerHTML = "<h1>Hello!</h1>"
+  }
+}
+```
+Then import this in your **application.js** file.
+```
+import Bridge from "../turbo/bridge.js";
+window.bridge = Bridge;
+```
+
+#### Add Rails Helpers
+In your Rails app, add the following helpers to your `application_helper.rb`
+```
+def turbo?
+  request.user_agent.include?("Turbo-")
+end
+
+def turbo_ios?
+  request.user_agent.include?("Turbo-iOS")
+end
+
+def turbo_android?
+  request.user_agent.include?("Turbo-Android")
+end
+```
+
+#### Add Authenticated Header
+Add the following **metatag** to your `<head>` so the app knows if a user is logged in or not.
+```
+<meta name="turbo:authenticated" content="<%= user_signed_in? %>">
+```
+
+#### Hide Page Navigation
+Since Turbo-iOS handles the native navbar, you don't need to show your page navigation anymore. 
+Add `unless turbo?` check around where you usually render your navbar in your Rails app. 
+```
+<% unless turbo? %>
+  <nav class="d-block">
+    <%= render 'partials/navbar' %>
+  </nav>
+<% end %>
+```
+
+#### Add Turbo Controller
+Add **turbo_controller.rb** which will return `turbo.json` used for rules and settings, here's a sample to get you started.
+```
+class TurboController < ApplicationController
+  def index
+    render json: {
+      "settings": {
+        "navbar": {
+          "background": "#888888",
+          "foreground": "#ffffff"
+        },
+        "tabbar": {
+          "background": "#888888",
+          "selected": "#ffffff",
+          "unselected": "#bbbbbb"
+        },
+        "tabs": [
+          {
+            "title": "Home",
+            "visit": "/",
+            "icon_ios": "house",
+            "protected": false
+          },
+          {
+            "title": "Profile",
+            "visit": "/profile",
+            "icon_ios": "building.2",
+            "protected": true
+          }
+        ],
+        "buttons": [
+          {
+            "path": "/",
+            "side": "left",
+            "icon_ios": "line.horizontal.3",
+            "script": "window.bridge.showMenu();",
+            "protected": false
+          },
+          {
+            "path": "/",
+            "side": "right",
+            "title": "Add",
+            "visit": "/posts/new",
+            "protected": true
+          }
+        ]
+      },
+      "rules": [
+        {
+          "patterns": [
+            "/new$",
+            "/edit$"
+          ],
+          "properties": {
+            "presentation": "modal"
+          }
+        },
+        {
+          "patterns": [
+            "/users/login"
+          ],
+          "properties": {
+            "presentation": "modal"
+          }
+        },
+        {
+          "patterns": [
+            "/users/logout"
+          ],
+          "properties": {
+            "presentation": "replace"
+          }
+        }
+      ]
+    }
+    end 
+end
+```
+To see all the available iOS icons you can use for navbar buttons or tabbar icons, visit [https://hotpot.ai/free-icons?s=sfSymbols](https://hotpot.ai/free-icons?s=sfSymbols).
+
+#### Add Turbo Route
+In your **routes.rb** add route pointing to `turbo#index`.
+```
+get 'turbo', to: "turbo#index", as: :turbo
+```
+
+#### Write Beautiful Ruby
+_That's it!_ Everything should now be configured including the app colors, tabs, navbar buttons, etc which are all driven from the `turbo.json` returned from `turbo_controller.rb`. 
+
+Now the app tabs and navbar buttons should appear according to the `protected` property if the user is authenticated or not. Your navbar buttons can either visit a page or trigger javascript on your server.
+
+The best part is you shouldn't need to write any Swift code, so you can focus on your backend Rails application. This is something I've dreamt about every since I first started using Rails, and it's now possible thanks to [Turbo-iOS](https://github.com/hotwired/turbo-ios)!
+
+If you find this project useful or have suggestions on improvements, please let me know!
